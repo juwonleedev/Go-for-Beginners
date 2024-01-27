869aecc156d266c190bdddfe6a966f9203e83359
Irene Lee <86876293+juwonleedev@users.noreply.github.com>
2024-01-27 23:19:57 +0900

    #1.0 + #.1.1

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main // for compile
 
+import (
+	"fmt" // package for format
+
+	"example.com/m/something"
+)
+
 func main(){ // crucial for go codes - entry point
-	print("Hello World!")
+	fmt.Print("Hello World!")
+	something.SayHello() // 대문자 함수 - 다른 패키지에서 import 해옴
+	// ctrl (command) + click -> 함수 조사 가능
 }
---
+++ something/something.go
@@ -0,0 +1,11 @@
+package something
+
+import "fmt"
+
+func hello(){
+	fmt.Println("Hello")
+}
+
+func SayHello(){
+	fmt.Println("SayHello")
+}
