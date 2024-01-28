01980cff998f384c37d806863640e1fac51a949b
Irene Lee <86876293+juwonleedev@users.noreply.github.com>
2024-01-28 22:56:41 +0900

    #1.2 Variables and Constants

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
-package main // for compile
+package main
 
-import (
-	"fmt" // package for format
+import "fmt"
 
-	"example.com/m/something"
-)
+func main() {
+	const name string = "nico" // go lang은 type language
 
-func main(){ // crucial for go codes - entry point
-	fmt.Print("Hello World!")
-	something.SayHello() // 대문자 함수 - 다른 패키지에서 import 해옴
-	// ctrl (command) + click -> 함수 조사 가능
+	var name2 string = "irene"
+	name2 = "nico"
+	fmt.Print(name2)
+
+	name3:="irene" // 축약: 자동 타입 찾기, func 내에서만 가능
+	fmt.Print(name3)
 }
