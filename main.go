1f80796f40fd19169ed643ed6ee54d1d43550396
Irene Lee <86876293+juwonleedev@users.noreply.github.com>
2024-01-28 23:10:24 +0900

    Functions part Two

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func multiply(a int,b int) int { // argument, return value type 지정
+	return a*b
+}
+
+// go -> multiple return이 가능함
+// go의 기본 라이브러리는 다양
+
+func lenAndUpper(name string) (int, string){
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string){ // 여러 개의 string 값 받음
+	fmt.Println(words) // array 형태로 출력됨
+}
 
 func main() {
-	const name string = "nico" // go lang은 type language
+	fmt.Print(multiply(2,3))
+	fmt.Print(lenAndUpper("irene"))
 
-	var name2 string = "irene"
-	name2 = "nico"
-	fmt.Print(name2)
-
-	name3:="irene" // 축약: 자동 타입 찾기, func 내에서만 가능
-	fmt.Print(name3)
+	totalLength, upperName := lenAndUpper("nico") // multiple value 저장
+	// totalLength, _ := lenAndUpper("nico") // 2번째 값 저장 하지 않음
+	fmt.Print(totalLength, upperName)
+	repeatMe("irene", "nico", "esther")
 }
